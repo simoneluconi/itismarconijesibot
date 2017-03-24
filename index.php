f0815a210a8ff2f14d97752e66274736e5745e26
Simone Luconi <luconi.simone@gmail.com>
2017-03-24 19:29:14 +0100

    Tentato fix della favicon

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
       <!-- Compiled and minified JavaScript -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-      <link rel="icon" href="/favicon.ico" type="image/x-icon">
+      <link rel="shortcut icon" href="/files/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="/files/favicon.ico" type="image/x-icon">
    </head>
    <body>
       <nav class="light-blue lighten-1" role="navigation">
