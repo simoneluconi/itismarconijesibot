a08c9ec5ec940dfbcff5037fda0a227235abcd94
Simone Luconi <luconi.simone@gmail.com>
2017-05-10 20:46:03 +0200

    Aggiunto timeout nel verificare se un sito è online

--- index.php
+++ index.php
@@ -130,6 +130,7 @@
                 curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
                 curl_setopt($curlInit,CURLOPT_HEADER,true);
                 curl_setopt($curlInit,CURLOPT_NOBODY,true);
+                curl_setopt($curlInit, CURLOPT_TIMEOUT, 10);
                 curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
 
                 //get answer
