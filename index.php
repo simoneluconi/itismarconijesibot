1cc8add8f1000a3d0c883ab8ef1c81341b530f0e
Simone Luconi <luconi.simone@gmail.com>
2018-03-13 12:08:28 +0100

    Risolto problema con alcuni eventi

--- index.php
+++ index.php
@@ -945,6 +945,7 @@
                 foreach ($eventi as & $evento) {
                     $data_inizio = $evento['data_inizio'];
                     $testo = $evento['testo'];
+                    $testo = $mysqli->real_escape_string($testo);
                     $result = $mysqli->query("SELECT * FROM db_eventi where evento='$testo' AND data_inizio='$data_inizio'", $link);
                     $num_rows = $result->num_rows;
                     if ($num_rows == 0) {
@@ -970,7 +971,6 @@
                             $message.= "\xF0\x9F\x95\x90 <b> $ora </b>\n";
                             $message.= "\xE2\x9C\x8F $testo \n";
                             if (!is_null($circolare_allegata))  $keyboard[] = array(array("text" => "\xF0\x9F\x93\x8E Circolare N. $numero_circolare", "callback_data" => "circolare://$numero_circolare"));
-                            $testo = $mysqli->real_escape_string($testo);
                             $evento_link = $evento['link'];
                             $result = $mysqli->query("INSERT INTO db_eventi (evento, data_inizio, ora, link, circolare_allegata) VALUES ('$testo', '$data_inizio', '$ora', '$evento_link', '$numero_circolare')");
 
@@ -986,7 +986,6 @@
                             $message = "\xF0\x9F\x93\x86 <b>" . $data_inizio . " - " . $data_fine . "</b>\n";
                             $message.= "\xE2\x9C\x8F $testo \n";
                             if (!is_null($circolare_allegata))  $keyboard[] = array(array("text" => "\xF0\x9F\x93\x8E Circolare N. $numero_circolare", "callback_data" => "circolare://$numero_circolare"));
-                            $testo = $mysqli->real_escape_string($testo);
                             $evento_link = $evento['link'];
                             $result = $mysqli->query("INSERT INTO db_eventi (evento, data_inizio, data_fine, link, circolare_allegata) VALUES ('$testo', '$data_inizio', '$data_fine', '$evento_link', '$numero_circolare')");
                             $keyboard[] = array(array("text" => "\xF0\x9F\x8C\x8D Guarda nel sito", "url" => $evento_link));                    
